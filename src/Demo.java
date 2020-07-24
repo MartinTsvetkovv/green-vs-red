d5f728a02d17861fbbde551336dc82ab36fff4d2
martin <martincl8310@gmail.com>
2020-07-24 15:34:31 +0300

    Code improvemets

--- src/Demo.java
+++ src/Demo.java
@@ -6,6 +6,7 @@ import io.impl.InputConsoleReader;
 public class Demo {
     public static void main(String[] args) {
 
+        System.out.println("Please enter two numbers for grid dimensions separate by comma and space (e.g. 3, 3):");
         InputReader reader = new InputConsoleReader();
         Printer printer = new ConsolePrinter();
         String[] gridDimensions = reader.readLine().split(", ");
@@ -13,10 +14,10 @@ public class Demo {
         int rows = Integer.parseInt(gridDimensions[0]);
         int cols = Integer.parseInt(gridDimensions[1]);
         int[][] matrix = new int[rows][cols];
-
+        System.out.println("PLease fill the grid. Your input should contain 0 or 1:");
         try {
             GreenVsRedGameCreator gridCreator = new GreenVsRedGameCreator(matrix, reader, printer);
-
+            System.out.println("PLease enter target cell coordinates and number N separate by comma and space (e.g. 1, 0, 10)");
             String[] targetCell = reader.readLine().split(", ");
 
             int targetCellRow = Integer.parseInt(targetCell[0]);
